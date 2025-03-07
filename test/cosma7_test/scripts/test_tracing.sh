f6755a3198e9320831fe68f9f24bb87998bca467
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2025-03-07 09:10:47 +0000

    Rename script directory

--- test/cosma7_test/scripts/test_tracing.sh
+++ test/cosma7_test/scripts/test_tracing.sh
@@ -4,7 +4,8 @@ module load gnu_comp/13.1.0 hdf5/1.12.2 openmpi/4.1.4
 source ../../../toolbox/swiftsim/openmpi-5.0.3-hdf5-1.12.3-env/bin/activate
 
 # Example path for COLIBRE test in COSMA7
-base_folder=/cosma8/data/dp004/dc-foro1/colibre/low_res_test/
+base_folder=/cosma7/data/dp004/dc-foro1/colibre/
+
 indir="${base_folder}/HBT" # HBT catalogue
 snap_files="${base_folder}/colibre_{snap_nr:04d}.hdf5" # SWIFT snapshots
 
