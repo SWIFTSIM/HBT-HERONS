3dc787dac1c65b41834df1cd17ff5d776b45ceeb
John Helly <j.c.helly@durham.ac.uk>
2025-05-01 11:44:38 +0100

    Add incomplete unit test for neighbour search
    This sets up a test case and runs the calculation but does not
    yet verify the result.

---
+++ unit_tests/test_neighbour_search.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <random>
+
+#include "config_parser.h"
+#include "snapshot.h"
+#include "geometric_tree.h"
+#include "verify.h"
+
+//
+// Make a snapshot subclass with random particle coordinates in a box
+//
+class RandomSnapshot_t : public Snapshot_t
+{
+
+private:
+
+  // A zero vector we can return a reference to
+  HBTxyz zero = {0., 0., 0.};
+
+  // Vector of particle coordinates
+  std::vector<HBTxyz> all_positions;
+
+public:
+
+  RandomSnapshot_t(HBTInt n, HBTReal boxsize, std::mt19937 &rng)
+  {
+    // Allocate storage
+    all_positions = std::vector<HBTxyz>(n);
+
+    // Assign random coordinates
+    std::uniform_real_distribution<HBTReal> dist(0, boxsize);
+    for(auto &pos : all_positions) {
+      pos[0] = dist(rng);
+      pos[1] = dist(rng);
+      pos[2] = dist(rng);
+    }
+  }
+
+  HBTInt size() const
+  {
+    return all_positions.size();
+  }
+
+  HBTInt GetId(const HBTInt index) const
+  {
+    return index;
+  }
+
+  const HBTxyz &GetComovingPosition(const HBTInt index) const
+  {
+    return all_positions[index];
+  }
+
+  const HBTxyz GetPhysicalVelocity(const HBTInt index) const
+  {
+    return zero;
+  }
+
+  HBTReal GetMass(const HBTInt index) const
+  {
+    return 1.0;
+  }
+
+  HBTReal GetInternalEnergy(HBTInt index) const
+  {
+    return 0.0;
+  }
+
+};
+
+
+int main(int argc, char *argv[])
+{
+  // Set up repeatable RNG
+  std::mt19937 rng;
+  rng.seed(0);
+
+  // Make a random snapshot
+  const HBTInt N = 1000;
+  const HBTReal boxsize = 1.0;
+  RandomSnapshot_t snap(N, boxsize, rng);
+
+  // Disable periodicity for now
+  HBTConfig.BoxSize = boxsize;
+  HBTConfig.BoxHalf = boxsize/2;
+  HBTConfig.PeriodicBoundaryOn = true;
+
+  // Build the octree
+  GeoTree_t tree;
+  tree.Build(snap, N);
+
+  // Make an array of centres for the neighbour search
+  const HBTInt Nsearch = 100;
+  std::vector<HBTxyz> centre(Nsearch);
+  std::uniform_real_distribution<HBTReal> dist(0, boxsize);
+  for(auto &pos : centre) {
+    pos[0] = dist(rng);
+    pos[1] = dist(rng);
+    pos[2] = dist(rng);
+  }
+
+  // Allocate storage for the results
+  std::vector<HBTInt> ngb_idx(Nsearch);
+
+  // Carry out the search
+  for(HBTInt i=0; i<Nsearch; i+=1) {
+    std::cout << i << std::endl;
+    ngb_idx[i] = tree.NearestNeighbour(centre[i], 0.01);
+  }
+
+  return 0;
+}
