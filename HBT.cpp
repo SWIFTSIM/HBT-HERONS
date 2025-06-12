4b3984cf582dfaba7cc3e5ac9db66491537a5577
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2025-06-12 12:52:05 +0100

    Small printing bug.

--- HBT.cpp
+++ HBT.cpp
@@ -42,10 +42,9 @@ int main(int argc, char **argv)
     cout << argv[0] << " run using " << world.size() << " mpi tasks";
 #ifdef _OPENMP
 #pragma omp parallel
-    cout << endl;
 #pragma omp master
-    cout << endl;
     cout << ", each with " << omp_get_num_threads() << " threads";
+    cout << endl;
 #endif
     cout << endl;
     cout << "Configured with the following data type sizes (bytes):" << endl;
