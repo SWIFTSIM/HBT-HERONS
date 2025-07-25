b2c89e90c3a9327d442c6fa56db18d3e43a2a668
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2025-07-25 15:36:21 +0100

    Make the code default to previous behaviour

--- src/config_parser.h
+++ src/config_parser.h
@@ -125,7 +125,7 @@ public:
     MajorProgenitorMassRatio = 0.8;
     BoundMassPrecision = 0.995;
     SourceSubRelaxFactor = 3.;
-    SubCoreSizeFactor = 0.25;
+    SubCoreSizeFactor = 0;
     SubCoreSizeMin = 20;
     TreeAllocFactor = 0.8; /* a value of 2 should be more than sufficient*/
     TreeNodeOpenAngle = 0.45;
@@ -153,7 +153,7 @@ public:
     DoNotSubsampleParticleBitMask = 0;
     for (int i : DoNotSubsampleParticleTypes)
       DoNotSubsampleParticleBitMask += 1 << i;
- 
+
     /* The value is negative to indicate whether the parameter has been set in the. If not,
      * we will default to a value of 1 if this is a swift HYDRO run. This way we reminder the
      * user to pre-process snapshots (toolbox/swiftsim/generate_splitting_information.py) */
