2d00045da87f4c601c4cfd85eeb3b7233ab60597
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2025-09-12 10:28:36 +0100

    Move helper functions to its own file

--- toolbox/swiftsim/pipeline/templates/generate_config.sh
+++ toolbox/swiftsim/pipeline/templates/generate_config.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
+
+. ../helper_functions.sh
 set -e
-
-# Parses a YAML file and returns each option as its own string. The levels of each
-# option are reflected by concatenating underscores between names.
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
-# Identifies whether a string contains a substring.
-stringContain() { case $2 in *$1* ) return 0;; *) return 1;; esac ;}
 
 # This is where the outputs should be placed
 BASE_PATH=$1
